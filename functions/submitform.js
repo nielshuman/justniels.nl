87a700a87f96db771388527bcd95ce7336881660
Niels <45306483+UltraNiels@users.noreply.github.com>
2020-08-17 12:33:34 +0200

    Update submitform.js

--- functions/submitform.js
+++ functions/submitform.js
@@ -1,15 +1,14 @@
 exports.handler = (event, context, resolve) => {
     if (event.httpMethod !== "POST") {
-        callback(null, ({
-                statusCode: 405,
-                body: "Method Not Allowed"
-            });
-        }
-        return;
-        const params = querystring.parse(event.body);
-        return {
-            statusCode: 200,
-            body: `${JSON.stringify(params)}`
-        };
+        callback(null, {
+            statusCode: 405,
+            body: "Method Not Allowed"
+        });
     }
+    return;
+    const params = querystring.parse(event.body);
+    return {
+        statusCode: 200,
+        body: `${JSON.stringify(params)}`
+    };
 }
