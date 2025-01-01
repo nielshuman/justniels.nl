448e0bbd90959230138e68177f9e588fbd1fd60b
Niels Huisman <nielsz@pm.me>
2025-01-01 19:36:30 +0100

    delete zooi

--- app/portfolio/creaties.js
+++ app/portfolio/creaties.js
@@ -10,22 +10,5 @@ export default function Creaties() {
                 Een website voor stichting HorsePowerRun die jaarlijks een toerrriet organiseert voor motorfietsen tot en met bouwjaar 1920.
                 </p>
             } />
-
-        <Creatie
-            title="BPM Beast"
-            image="/bpmbeast.webp"
-            url="https://bpmbeast.justniels.nl/"
-            description={<p>
-                Muzikale metronoom op basis van je eigen spotifybibliotheek.
-            </p>
-            } />
-        <Creatie
-            title="Het Spel Spel"
-            image="/hetspelspel.webp"
-            url="https://hetspelspel.netlify.app/"
-            description={<p>
-                Een spel waarbij je moet spellen. Dit heb ik ooit toen ik 12 was voor mijn broertje gemaakt zodat hij op een leuke manier zijn woordenlijsten kon oefenen.
-            </p>}
-        /> 
     </>
 }
