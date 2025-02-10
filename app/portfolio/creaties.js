f630cfd3088bad6e6b7cd6037bad7cb6d66095c0
Niels Huisman <nielsz@pm.me>
2025-02-10 20:25:26 +0100

    baa[ps

--- app/portfolio/creaties.js
+++ app/portfolio/creaties.js
@@ -7,7 +7,7 @@ export default function Creaties() {
             image={"/hpr.png"}
             url="https://horsepowerrun.nl/"
             description={<p>
-                Een website voor stichting HorsePowerRun die jaarlijks een toerrriet organiseert voor motorfietsen tot en met bouwjaar 1920.
+                Een website voor stichting HorsePowerRun die jaarlijks een toerrit organiseert voor motorfietsen tot en met bouwjaar 1920.
                 </p>
             } />
     </>
