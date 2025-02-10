d9d941549d5adfde7dba65f367c93edf32437e67
Niels Huisman <nielsz@pm.me>
2025-02-10 20:25:06 +0100

    aa

--- app/contact/page.js
+++ app/contact/page.js
@@ -7,6 +7,8 @@ export default function Keys() {
             <p>
                 JustNiels <br />
                 <a href="mailto:info@justniels.nl"> info@jusniels.nl </a> <br />
+                <a href="tel:+31623579871"> +31 6 23579871 </a>
+                <a href="https://api.whatsapp.com/send/?phone=31623579871"> WhatsApp </a>
             </p>
             <p>
                 Petrus Campersingel 19 <br />
