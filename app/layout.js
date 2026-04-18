70041ea5e6e0c6700c59a89c376de62c9d23869a
Niels Huisman <nielsz@pm.me>
2026-04-18 10:43:03 +0200

    De groeten van de stemmen in mn hoofd

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ import Tracker from "@/components/Tracker";
 import "./globals.css";
 
 export const metadata = {
-  title: "Justniels.nl",
-  description: "Niels Huisman",
+  title: "Onder constructie - JustNiels",
+  description: "Deze website wordt momenteel ontwikkeld door JustNiels. Kijk op JustNiels.nl voor meer informatie over mijn diensten op het gebied van websiteontwikkeling en beheer.",
 };
 
 export default function RootLayout({ children }) {
@@ -12,11 +12,11 @@ export default function RootLayout({ children }) {
       <body>
         {children}
       </body>
-      <Tracker 
+      {/* <Tracker 
         url='https://analytics.caprover.justniels.nl' 
         domainId='658ff237-a289-486c-b962-596fe199c1be' 
         options={{detailed: true}}
-      />
+      /> */}
     </html>
   );
 }
